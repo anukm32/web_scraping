f3214acba9ad0ba8c63b89010b0b72369f79e59e
Anu KM <anumol.km32@gmail.com>
2021-08-14 10:19:23 +0400

    commit

--- scraping.py
+++ scraping.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 website_url="https://infopark.in/companies/jobs/kochi-phase-2?"
 key_words=["python"]
 res=requests.get(website_url)
-print(res.text)
+
 soup=BeautifulSoup(res.text,'lxml')
 jobs=soup.find_all("div",{"class":"row company-list joblist"})
 
@@ -11,6 +12,6 @@ for job in jobs:
    title_element= job.find("a")
    title=title_element.text
    link=title_element["href"]
-   company_name=job.find("div",{"class":"jobs-comp-name "}).text
-   post_date=job.find("div",{"class":"job-date "}).text
-   print(title_element,company_name, post_date)
+   company_name=job.find("div",{"class":"jobs-comp-name "})
+   post_date=job.find("div",{"class":"job-date "})
+   print (title_element.text.strip())
